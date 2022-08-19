f10d426ada56bd6cdc6230f16da5941f12c0d0ec
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-19 03:04:19 -0500

    inicio header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 //Components import
 import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Banner />
     </div>
   );
