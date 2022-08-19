92c281d33f70135d3775214a949a59a5e69d7b78
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-19 03:34:41 -0500

    cambios navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 //Components import
+import { useState } from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
 
 function App() {
+  const [navegar, setNavegar] = useState(false);
   return (
     <div>
-      <Header />
+      <Header setNavegar={setNavegar} />
       <Banner />
+      <div
+        className={`${
+          navegar ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-48 transition-all`}
+      >
+        <NavMobile setNavegar={setNavegar} />
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Logo from "../assets/img/logo.svg";
+import Nav from "./Nav";
+//icononos
+import { HiMenu } from "react-icons/hi";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ setNavegar }) => {
+  return (
+    <header className="py-6">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <a href="">
+            <img className="h-[30px]" src={Logo} />
+          </a>
+          {/* nav */}
+          <Nav />
+          <HiMenu
+            onClick={() => setNavegar(true)}
+            className="lg:hidden text-3xl text-white cursor-pointer "
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
