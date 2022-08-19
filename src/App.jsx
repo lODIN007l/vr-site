41d0f47237ede8e3f48ef3eeb912eb10ce17eb2d
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-19 03:40:35 -0500

    anvbar section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import NavMobile from "./components/NavMobile";
 function App() {
   const [navegar, setNavegar] = useState(false);
   return (
-    <div>
+    <div className=" relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px]  before:hidden before:lg:flex">
       <Header setNavegar={setNavegar} />
       <Banner />
       <div
