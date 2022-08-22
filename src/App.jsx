6ac4840059b70c63df755ad623159f9ec536f0f7
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-21 19:42:51 -0500

    renombre de sitio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //Components import
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import Experience from "./components/Experience";
 import Header from "./components/Header";
@@ -8,9 +8,17 @@ import Video from "./components/Video";
 import Headsets from "./components/Headsets";
 import Testimonial from "./components/Testimonial";
 import Explore from "./components/Explore";
+//animaciones
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   const [navegar, setNavegar] = useState(false);
+  //configuracion inicial aos
+  useEffect(() => {
+    Aos.init({ duration: 2500, delay: 400 });
+  }, []);
+
   return (
     <div className=" relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px]  before:hidden before:lg:flex">
       <Header setNavegar={setNavegar} />
