980c1323856847512c11b22512e9d8d41783397b
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-21 19:16:12 -0500

    envio sliders

--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -6,20 +6,35 @@ import { Pagination } from "swiper";
 import { slider } from "../data";
 const TestimonialSlider = () => {
   return (
-    <Swiper>
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 18,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+    >
       {slider.map((slide, index) => {
         //destruture slide
         const { message, image, name, email } = slide;
         return (
           <SwiperSlide className="rounded-lg p-4 lg:p-6 min-h-[240px] bg-gradient-to-t from-[#130613] via-[#2A1428] to-[#331A35]">
-            <p>{message}</p>
+            <p className="mb-8 min-h-[100px] text-[15px]">{message}</p>
             <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
-              <div>
-                <img src={image} alt="" />
+              <div className="w-14">
+                <img className="mb-3 md:mb-0" src={image} alt="" />
               </div>
               <div>
-                <div>{name}</div>
-                <div>{email}</div>
+                <div className="font-medium text-base ">{name}</div>
+                <div className="font-medium text-rose-600"> {email}</div>
               </div>
             </div>
           </SwiperSlide>
